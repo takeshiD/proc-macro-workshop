8ec4331ca01d9c662557bf0fad4fedb11adcca66
takeshid <goriponikeike55@gmail.com>
2025-12-14 22:30:58 +0900

    derive_builder: passed test2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -12,12 +12,36 @@ pub fn derive(input: TokenStream) -> TokenStream {
 mod macro_impl {
     use proc_macro2::TokenStream;
     use quote::format_ident;
+    use syn::{spanned::Spanned, Data, DeriveInput, Fields, Index};
     pub fn builder_impl(input: syn::DeriveInput) -> syn::Result<TokenStream> {
-        let ident = input.ident.clone();
+        let span = input.span();
+        let DeriveInput { data, ident, .. } = input;
+        let Data::Struct(ds) = data else {
+            return Err(syn::Error::new(span, "expected struct"));
+        };
         let builder_struct = format_ident!("{}Builder", ident);
+        let fields = match ds.fields {
+            Fields::Named(f) => f.named,
+            _ => {
+                return Err(syn::Error::new(span, "expected named field"));
+            }
+        };
+        let builder_fields = fields.iter().map(|f| {
+            let ident = &f.ident;
+            let ty = &f.ty;
+            quote::quote! {
+                #ident: ::core::option::Option<#ty>
+            }
+        });
+        let builder_fields_init = fields.iter().map(|f| {
+            let ident = &f.ident;
+            quote::quote! {
+                #ident: None
+            }
+        });
         Ok(quote::quote! {
             struct #builder_struct {
-                hello: String,
+                #(#builder_fields,)*
             }
             impl #builder_struct {
                 pub fn print_derive_input(&self) {
@@ -27,7 +51,7 @@ mod macro_impl {
             impl #ident {
                 pub fn builder() -> #builder_struct {
                     #builder_struct {
-                        hello: "Hello".to_string(),
+                        #(#builder_fields_init,)*
                     }
                 }
             }
